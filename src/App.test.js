4df5ac3b0d2a3223a51d97a530b3e9d24f26c68c
unknown <Simon@smashmail.net>
2024-06-05 14:34:32 +0100

    removed comment - added new comment - let'see

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+//hi, this is a comment by simon
 
 test('renders VAT Calculator text', () => {
   render(<App />);
